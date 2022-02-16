8410bdef221e14a4da4858d66c30505a3051a2e8
haelina <56834622+haelina@users.noreply.github.com>
2022-02-16 17:42:31 +0200

    Add fixed build

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,8 +1,13 @@
 const config = {
-  host: process.env.HOST,
+  /*host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  database: process.env.DATABASE,*/
+  host: "eu-cdbr-west-02.cleardb.net",
+  user: "b1b71d9bad2f18",
+  password: "04c8ca3e",
+  database: "heroku_88cd6f3f2aaea0e",
+
   connectionLimit: 10,
 };
 
