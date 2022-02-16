38aa7e2d1fc403877c4fd395f1f19e2d3cf120ce
haelina <56834622+haelina@users.noreply.github.com>
2022-02-16 18:48:38 +0200

    Add connection functions like post and delete

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -18,11 +18,73 @@ const connectionFunctions = {
       if (connection) {
         connection.query("SELECT * FROM person", (error, results) => {
           if (error) {
-            console.log(error);
+            reject(error);
           } else {
             resolve(results);
           }
         });
+      } else {
+        reject(Error);
+      }
+    });
+  },
+  findById: (id: number) => {
+    return new Promise<Person[]>((resolve, reject) => {
+      if (connection) {
+        connection.query(
+          "SELECT * FROM person WHERE id=?",
+          id,
+          (error, result) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(result);
+            }
+          }
+        );
+      } else {
+        reject(`No connection. Could not fetch person with id ${id}`);
+      }
+    });
+  },
+
+  addPerson: (p: Person) => {
+    return new Promise<Person>((resolve, reject) => {
+      if (connection) {
+        connection.query(
+          "INSERT INTO * FROM person (firstName, lastName, age) VALUES (?, ?, ?)",
+          (p.firstName, p.lastName, p.age),
+          (error, result) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(result.insertId);
+            }
+          }
+        );
+      } else {
+        reject(`No connection. Could not add new person.`);
+      }
+    });
+  },
+  deleteById: (id: number) => {
+    return new Promise<string>((resolve, reject) => {
+      if (connection) {
+        connection.query(
+          "DELETE FROM person WHERE id=?",
+          id,
+          (error, result) => {
+            if (error) {
+              reject(error);
+            } else if (result.affectedRows === 0) {
+              reject(`Id ${id} not found. Abort deleting.`);
+            } else {
+              resolve(`Id ${id} deleted successfully.`);
+            }
+          }
+        );
+      } else {
+        reject(`No connection. Could not add new person.`);
       }
     });
   },
