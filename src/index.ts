162a10ff254b9b5d7f18d30b8b43a2f23f17ad09
haelina <56834622+haelina@users.noreply.github.com>
2022-02-16 16:25:02 +0200

    Make react frontend work

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,32 @@
 import express, { Application, Request, Response } from "express";
 import routes from "./api/routes";
 import cors from "cors";
+import path from "path";
+
+import database from "./database/connection";
 
 const app: Application = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(cors(), express.json());
+
+// connecting front to backend
+app.use(express.static("./build"));
+
 app.use("/api/people", routes);
 
 // define a route handler for the default home page
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world!");
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
 // start the Express server
 const server = app.listen(port, async () => {
   try {
-    console.log(`server started at http://localhost:${port}`);
+    await database.connect();
+    console.log(`Connection successful, server started on port ${port}`);
   } catch (e) {
     console.log(e);
+    server.close();
   }
 });
